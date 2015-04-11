46c3d90272387d5ffd7f6350fad34ea3fc94a860
Ju Hyeon Park <wheredidjupark@gmail.com>
2015-04-10 18:56:31 -0700

    complete stringifyJSON.js

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -22,21 +22,17 @@ var stringifyJSON = function(obj) {
 		return obj.toString();
 		break;
 	case("undefined"):
+		return "";
 		break;
 	case("symbol"):
+		return "";
 		break;
 	case("function"):
+		return "";
 		break;
 	case("object"):
 //put everything here
-	default:
-		break;
-
-  }
-
-  if(typeof obj === "object") {
-  	
-  	//null
+ 	//null
   	if(obj === null) {
   		return 'null';
   	}
@@ -44,9 +40,23 @@ var stringifyJSON = function(obj) {
   	//Array
 
   	if(Array.isArray(obj)) {
- 
+  		var obj_cpy = obj;
+  		var result = '';
+  			for (var i = 0; i<obj_cpy.length;i++){
+  				if (i > 0){
+  					result += ',';
+  				}
+  				result += stringifyJSON(obj_cpy[i]);
+  			}
+  		return "["+ result +"]";
+ /*
   		var result ='';
 
+  		if(obj.length ===1){
+  			result += stringifyJSON(obj[0]);
+   		  console.log(result);
+
+  		}
   		if(obj.length >1){
 
   		for(var i=0; i < obj.length -1;) {
@@ -60,12 +70,11 @@ var stringifyJSON = function(obj) {
 
   		}
 
-  		if(obj.length ===1){
-  			result = stringifyJSON(obj[0]);
-  		}
+
+
 
   		return '[' +result + ']';
-
+*/
 /*
   		if(obj.length > 1){
   			var temp = stringify(obj.shift());
@@ -92,20 +101,26 @@ var stringifyJSON = function(obj) {
   		else{
   		var result ="";
   		var index = 0;
-
   		for(var key in obj){
+  			if(stringifyJSON(obj[key]) !== ""){
+
   			result += stringifyJSON(key) + ":" + stringifyJSON(obj[key]); 
   			if(index < Object.keys(obj).length-1) {
   				result += ","
   			}
   			index++;
+  			}
+
 
   		}
 
-  	   return "{ " +result + "}";
+  	   return "{" +result +"}";
 }
+	default:
+		break;
 
   }
+
 
 
 
