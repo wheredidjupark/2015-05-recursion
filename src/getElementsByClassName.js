10fb2056cedc7096570d8e29d4774358e59723f7
Ju Hyeon Park <wheredidjupark@gmail.com>
2015-04-11 14:00:40 -0700

    complete getElementsByClassName.js

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,63 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-){
+var getElementsByClassName = function(className){
   // your code here
+  
+  var result = [];
+
+
+
+  var containsClass = function(node, className){
+  	if(node.classList === undefined){
+  		return false;
+  	}
+  	else
+  	{
+  		var classList = node.classList;
+  		var classListLength = classList.length;
+  		for(var i=0; i < classListLength; i++){
+  			if(classList[i] === className){
+  				return true;
+  			}
+  		}
+  		return false;
+  	}
+  };
+  
+
+var investigate = function(context, className){
+	var target; 
+	if(context.childNodes){
+		/*
+		if(containsClass(context, className)){
+			result.push(context);
+		}
+		*/
+
+		if(context.childNodes){
+			target = context.childNodes;
+			for(var i=0; i < target.length; i++){
+				if(containsClass(target[i],className)){
+					result.push(target[i]);
+				}
+				investigate(target[i], className);
+			}	
+		}
+
+	}
+	else
+	{
+		return;
+	}
+}
+
+investigate(document, className); 
+  //You should use document.body, element.childNodes, and element.classList
+  //first target body as the target
+  //then inspect each child node in the body
+  	//change 
+  	//change target as the child node 
+  return result;
+  
 };
